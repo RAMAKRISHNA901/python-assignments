9779e4f83930065c7297d053a0f13e3f550c0ef6
RAMAKRISHNA901 <150525787+RAMAKRISHNA901@users.noreply.github.com>
2023-11-14 23:24:28 +0530

    Add files via upload

---
+++ assignment1/assignment1.py
@@ -0,0 +1,18 @@
+# write a program biggest given 2 num
+b=float(input("enter the first number:"))
+c=float(input("enter a second number:"))
+if b>c:
+    print('biggest number:,b')
+else:
+    print('biggest number:,c')
+print('i find this')
+    
+    
+
+    
+
+
+
+
+
+
