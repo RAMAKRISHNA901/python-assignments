f260dc7a4025a33af9e85ba9fa2be0ee91a91f06
RAMAKRISHNA901 <150525787+RAMAKRISHNA901@users.noreply.github.com>
2023-11-21 14:20:42 +0530

    Add files via upload

---
+++ elements of lists and functions of lists.21-11-23/for loop and while loop list .py
@@ -0,0 +1,95 @@
+# by using while loop
+x=[10,20,30,40,50,]
+print(x)
+i=0
+while i<len(x):
+    print(x[i])
+    i=i+1
+#
+x=[11,12,13,14,15,16,17]
+print(x)
+i=0
+while i<len(x):
+    print(x[i])
+    i=i+1
+
+# 
+l=[3,4,5,6,7,8,9]
+i=0
+while i<len(l):
+    print(l[i])
+    i=i+1
+
+# by using for loop.
+l=[10,20,30,40,50,60]
+for i in l:
+    if i%2==0:
+        print(i)  
+#by using for loop even numbers.  
+l=[12,13,14,15,16,17,18,19]   
+for i in l:
+    if i%2==0:
+        print(i)
+    
+# lists of elements.
+l=["a","m","a","n"]
+for i in l:
+    print(i)
+  
+#
+l=["a","m","a","n"]  
+a=len(l)
+for i in range(a):
+    print(l[i],"is a available at +ve index is",i,"and at -ve index is",i-a)
+
+# append()
+# by using for loop.
+x=[]
+for i in range(24):
+    l.append(i)
+print(l)
+
+x=[]
+for i in range(21):
+    if i%2==1:
+        l.append(i)
+print(i)
+
+#
+# by using while loop.
+l=[10,20,30,40,50]
+print(len(11))
+i=1
+while i<len(11):
+    if i%2==1:
+        print(i)
+    i=i+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ elements of lists and functions of lists.21-11-23/functions of lists.py
@@ -0,0 +1,39 @@
+# functions of lists.
+#len()
+l=[10,20,30,40,50,60,70]
+print(len(l))
+print()
+
+#count()
+l=[10,20,30,40,50]
+print(l.count(50))
+
+# index()
+l=[10,20,30,40,50]
+print(l.index(40))
+
+#append ()
+l=[10,20,30,40,50]
+l.append(45)
+print(l)
+
+#insert()
+l=1[10,20,30,40,50,60,70,]
+l.insert(4,50)
+print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
