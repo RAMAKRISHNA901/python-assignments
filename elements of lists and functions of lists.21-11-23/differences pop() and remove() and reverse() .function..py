22ce79dcb076b2dd48f94564338271d8844fc2fb
RAMAKRISHNA901 <150525787+RAMAKRISHNA901@users.noreply.github.com>
2023-11-22 11:37:53 +0530

    Add files via upload

---
+++ elements of lists and functions of lists.21-11-23/differences pop() and remove() and reverse() .function..py
@@ -0,0 +1,26 @@
+## difference between pop() and remove()
+# remove()
+#syntax:l.remove(elements)
+#*remove functions the first occurencel=[10,20,30,40,50,70,80,]
+#*it will return element which removed from the list
+
+# pop()
+#syntax:l.pop(index,numers)
+# *the pop function removes the lasy element based on the given index.
+# *remove the item given position in the list and return it.
+#*it will return elemet.s of the specified.
+#remove the first list from the list values mentioned.
+#*return type  is none.
+
+
+# reverse()
+# reverse method returns iterator accesses given reverse order
+# return type is reserved iterator object
+# reserved function is suppout is string,tuple,dictonary.
+# syntax: l.reverse()
+
+
+
+
+
+
