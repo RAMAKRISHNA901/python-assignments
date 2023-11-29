16161e7a93c9ec504aea65a81f818c33fe47ba4a
RAMAKRISHNA901 <150525787+RAMAKRISHNA901@users.noreply.github.com>
2023-11-29 14:04:06 +0530

    Add files via upload

---
+++ set.py differences pop() and discard() and remove().py
@@ -0,0 +1,25 @@
+# differences between pop() and remove() and dicard()
+
+# pop()
+#1.remove on element from the set.
+#2.syntax: set.pop()
+#3.indexing and duplicate vaues are not allowed 
+#4.if the set is empty calling pop.
+#5.pop is not suppourted forzen set .
+#6.remove the multiple elements in pop.
+
+
+# discard()
+# the discard method remove the specified item from the set.
+# duplicate values to remove all duplicate values from the set
+#hetrogenous values are allowed.
+#duplicate values are not allowed
+#dicard works for mutable and immutable objects are similar .
+#syntax: set.discard()
+
+#remove ()
+#delete aspecified element from the set
+#syntax:set.remove()
+#duplicate values are not allowed
+#hetrogenous values are allowed
+# elements specified in remove() does not exit in the set,it raise key error.
