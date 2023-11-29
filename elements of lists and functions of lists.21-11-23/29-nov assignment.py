dc88fa9ff397b6d47fad40aa6d9063b180ef776e
RAMAKRISHNA901 <150525787+RAMAKRISHNA901@users.noreply.github.com>
2023-11-29 10:29:58 +0530

    Add files via upload

---
+++ elements of lists and functions of lists.21-11-23/29-nov assignment.py
@@ -0,0 +1,53 @@
+#write a pro to take tuple of numbers from keyboard and print Sum,avg.
+tuple=(10,20,30,40,50,60,70,80,90)
+print(len(tuple))
+avg=len(tuple)
+sum=0
+for i in tuple:
+    sum=sum+i
+print(sum/avg)
+
+#
+t=(10,20,30,40,50,60,70,80,90,100,101)
+sum=0
+for i in t:
+    sum=sum+i
+print(sum)
+
+t=(10,20,30,40,50,60,70,80,90,100,101)
+sum=0
+avg=len(t)
+for i in t:
+    sum=sum+i
+print(sum/avg)
+
+
+
+#write down assigment on each methods with 5 diffrent examples of pop()and discard() and remove()
+
+x={10,20,30,40,50,60}
+x.pop()
+print(x)
+#
+x={200,300,400,500,600}
+x.pop()
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
