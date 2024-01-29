d6090870e635e168244961a8cc35ee7abd2fa9ba
RAMAKRISHNA901 <150525787+RAMAKRISHNA901@users.noreply.github.com>
2024-01-29 08:11:43 +0530

    Add files via upload

---
+++ ststic variable jan 17.py
@@ -0,0 +1,41 @@
+import sys
+class customer:
+    bankname="SBI"
+    def __init__(self,name,balance=0.0):
+        self.name=name
+        self.balance=balance
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        print("the balance after the deposit is :",self.balance)
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("insufficient amount, please withdraw with in the balance amount")
+            sys.exit()
+        self.balance=self.balance-amount
+        print("the balance after the withdraw is ",self.balance)
+
+print("welcome to ",customer.bankname)
+name=input("Enter your name :ram ")
+c=customer(name)
+while True:
+    print("d-deposit \n w- withdraw \n e- exit")
+    option =input(" choose your option ") 
+    if option=="d":
+        amount=int(input("enter the amount to deposit : "))#1000
+        c.deposit(amount)
+    elif option=="w":
+        amount=int(input(" enter the withdraw amount :"))
+        c.withdraw(amount)
+    elif option=="e":
+        print("thank you for banking with us ")
+        sys.exit()
+    else:
+        print("invaild option, please select correct option ")
+    
+        
+        
+        
+        
+        
+        
+        
